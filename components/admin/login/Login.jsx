60c317748039c77e3d2b68ab5c6767c65bef5433
latif83 <latifm8360@gmail.com>
2024-03-06 19:13:41 +0000

    updates

--- components/admin/login/Login.jsx
+++ components/admin/login/Login.jsx
@@ -1,6 +1,8 @@
-"use client"
+"use client";
 import { useLogin } from "@/providers/LoginContext";
 import { useToaster } from "@/providers/ToasterContext";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
@@ -8,8 +10,8 @@ export const Login = () => {
   const [formData, setFormData] = useState(null);
   const [sendData, setSendData] = useState(false);
   const [loading, setLoading] = useState(false);
-  const {login} = useLogin();
-  const {showToast} = useToaster();
+  const { login } = useLogin();
+  const { showToast } = useToaster();
 
   const submitData = (formData) => {
     setFormData(formData);
@@ -33,16 +35,15 @@ export const Login = () => {
       }
     };
 
-    if(sendData){
-        sData()
-        setSendData(false)
+    if (sendData) {
+      sData();
+      setSendData(false);
     }
-
   }, [sendData]);
 
   return (
-    <section style={{height:'100svh'}} className="bg-gray-50">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+    <section style={{ height: "100svh" }} className="bg-gray-50">
+      <div className="flex flex-col items-center justify-center sm:px-6 px-3 h-full sm:h-auto py-8 mx-auto md:h-screen lg:py-0">
         <Image
           className="w-12 h-12 mr-2 mb-3"
           src="/logo.jpg"
@@ -67,7 +68,7 @@ export const Login = () => {
                   type="text"
                   name="username"
                   id="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
                   placeholder="johndoe"
                   required=""
                 />
@@ -84,7 +85,7 @@ export const Login = () => {
                   name="password"
                   id="password"
                   placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
                   required=""
                 />
               </div>
@@ -95,7 +96,7 @@ export const Login = () => {
                       id="remember"
                       aria-describedby="remember"
                       type="checkbox"
-                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300"
+                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300"
                       required=""
                     />
                   </div>
@@ -107,7 +108,7 @@ export const Login = () => {
                 </div>
                 <a
                   href="#"
-                  className="text-sm font-medium text-primary-600 hover:underline"
+                  className="text-sm font-medium text-blue-600 hover:underline"
                 >
                   Forgot password?
                 </a>
@@ -115,9 +116,13 @@ export const Login = () => {
 
               <button
                 type="submit"
-                className="w-full text-white hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-primary-600"
+                className="w-full text-white hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600"
               >
-                Sign in
+                {loading ? (
+                  <FontAwesomeIcon icon={faSpinner} spin className="text-lg" />
+                ) : (
+                  "Sign in"
+                )}
               </button>
             </form>
           </div>
